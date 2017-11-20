446bc6f0b3807e5840911ac20de1edddac86b428
slamdon <knirosoft@hotmail.com>
2017-11-20 15:02:53 +0800

    update tap-counter

--- 1-Tap-Counter/app/src/main/java/devdon/com/tap_counter/MainActivity.kt
+++ 1-Tap-Counter/app/src/main/java/devdon/com/tap_counter/MainActivity.kt
@@ -2,11 +2,50 @@ package devdon.com.tap_counter
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
+
+import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
+
+    private var currentNumber = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupView()
     }
+
+    private fun setupView() {
+        this.tapButton.setOnClickListener{
+            currentNumber += 1
+            numberTextView.text = currentNumber.toString()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        this.menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item?.itemId) {
+            R.id.menuReset -> {
+                this.currentNumber = 0
+                this.numberTextView.text = "0"
+            } else -> this.toast("Error while item selected!")
+        }
+
+
+        return super.onOptionsItemSelected(item)
+    }
+
+
+
+
+
 }
--- readme.md
+++ readme.md
@@ -1,2 +1,10 @@
 # Kotlin Playground
 學習通過 Kotlin 開發 Android App
+
+## 1 - Tap Counter
+
+2017.11.20
+
+![TapCounter](resources/tap-counter.gif)
+
+
