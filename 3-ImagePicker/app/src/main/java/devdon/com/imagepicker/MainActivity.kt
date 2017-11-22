1f0c18eb88616cdc60bea08874ab0d8cb3a07ed8
slamdon <knirosoft@hotmail.com>
2017-11-22 20:41:42 +0800

    add ImagePicker

---
+++ 3-ImagePicker/app/src/main/java/devdon/com/imagepicker/MainActivity.kt
@@ -0,0 +1,109 @@
+package devdon.com.imagepicker
+
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.view.View
+import android.widget.ImageView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val PERMISSION_CAMERA_REQUEST_CODE = 100
+    private val ACTION_CAMERA_REQUEST_CODE = 200
+    private val ACTION_ALBUM_REQUEST_CODE = 300
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        cameraAppButton.setOnClickListener(cameraAppButtonHandler)
+        albumAppButton.setOnClickListener(albumAppButtonHandler)
+
+        imageView.scaleType = ImageView.ScaleType.CENTER_CROP
+
+    }
+
+    // 通過 intent 使用 Camera
+    private fun takeImageFromCameraWithIntent() {
+        println("take image from camera")
+
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(intent, ACTION_CAMERA_REQUEST_CODE)
+    }
+
+    // 通過 intent 使用 album
+    private fun takeImageFromAlbumWithIntent() {
+        println("take image from album")
+
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, ACTION_ALBUM_REQUEST_CODE)
+    }
+
+    private val cameraAppButtonHandler = View.OnClickListener { view ->
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            println("請求照相機使用權限")
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), PERMISSION_CAMERA_REQUEST_CODE)
+
+        } else {
+            println("使用相機")
+            takeImageFromCameraWithIntent()
+
+        }
+    }
+
+    private val albumAppButtonHandler = View.OnClickListener { view ->
+        takeImageFromAlbumWithIntent()
+
+    }
+
+    private fun displayImage(bitmap: Bitmap) {
+        imageView.setImageBitmap(bitmap)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        println("收到 result code $requestCode")
+
+        when(requestCode) {
+            ACTION_CAMERA_REQUEST_CODE -> {
+                if(resultCode == Activity.RESULT_OK && data != null){
+                    displayImage(data.extras.get("data") as Bitmap)
+                }
+            }
+
+            ACTION_ALBUM_REQUEST_CODE -> {
+                if(resultCode == Activity.RESULT_OK && data != null){
+                    val resolver = this.contentResolver
+                    val bitmap = MediaStore.Images.Media.getBitmap(resolver, data?.data)
+                    displayImage(bitmap)
+
+                }
+            }
+            else -> {
+                println("no handler onActivityReenter")
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == PERMISSION_CAMERA_REQUEST_CODE){
+            if(grantResults.all { it == PackageManager.PERMISSION_GRANTED}){
+                takeImageFromCameraWithIntent()
+            } else {
+                println("拒絕相機使用權限")
+            }
+        }
+    }
+
+}
